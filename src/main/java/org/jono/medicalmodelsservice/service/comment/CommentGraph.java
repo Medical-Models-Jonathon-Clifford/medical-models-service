b2186d635ec9a6fcadda4de34ad16a194a1fcdd9
JonathonClifford <jono.cliff@gmail.com>
2025-01-15 09:41:09 +0000

    refactor: Removing generic type declaration from CommentGraph. Adding var to SimpleMailMessage bean. (#10)
    Reviewed-on: http://gitea.local/medical-models/medical-models-service/pulls/10
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

--- src/main/java/org/jono/medicalmodelsservice/service/comment/CommentGraph.java
+++ src/main/java/org/jono/medicalmodelsservice/service/comment/CommentGraph.java
@@ -4,13 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.jono.medicalmodelsservice.model.Comment;
 import org.jono.medicalmodelsservice.model.CommentChild;
 import org.jono.medicalmodelsservice.service.GraphBuilder;
-import org.jono.medicalmodelsservice.service.NodeData;
-import org.jono.medicalmodelsservice.service.NodeRelationship;
 
 import java.util.List;
 
 @Slf4j
-public class CommentGraph<T, R extends NodeRelationship, S extends NodeData> {
+public class CommentGraph {
 
     public static List<CommentNode> buildGraph(List<Comment> commentList,
                                                List<CommentChild> commentChildList) {
