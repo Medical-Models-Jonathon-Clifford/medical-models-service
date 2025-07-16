489dce65f0c3384cb8a90c527b9b6fe3f89594dd
JonathonClifford <jono.cliff@gmail.com>
2025-07-16 10:16:01 +0000

    refactor: MM-89 Add final and fix indent in tests. (#41)
    Reviewed-on: https://gitea.busybunyip.com/medical-models/medical-models-service/pulls/41
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

--- src/test/java/org/jono/medicalmodelsservice/service/comment/CommentGraphTest.java
+++ src/test/java/org/jono/medicalmodelsservice/service/comment/CommentGraphTest.java
@@ -20,179 +20,179 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 class CommentGraphTest {
 
-    @Test
-    public void noComments() {
-        List<Comment> emptyCommentList = Collections.emptyList();
-        List<CommentChild> emptyCommentChildList = Collections.emptyList();
+  @Test
+  public void noComments() {
+    final List<Comment> emptyCommentList = Collections.emptyList();
+    final List<CommentChild> emptyCommentChildList = Collections.emptyList();
 
-        List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(emptyCommentList, emptyCommentChildList);
+    final List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(emptyCommentList, emptyCommentChildList);
 
-        assertThat(actualCommentNodes.size()).isEqualTo(0);
+    assertThat(actualCommentNodes.size()).isEqualTo(0);
+  }
+
+  @Test
+  public void oneCommentNoChildren() {
+    final List<Comment> oneCommentList = createCommentList("41");
+    final List<CommentChild> emptyCommentChildList = Collections.emptyList();
+
+    final List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(oneCommentList, emptyCommentChildList);
+
+    assertThat(actualCommentNodes.size()).isEqualTo(1);
+    assertThat(actualCommentNodes.getFirst().getComment())
+        .extracting("id", "body", "documentId")
+        .containsExactly("41", "Test comment 41", "97");
+    assertThat(actualCommentNodes.getFirst().getChildren().size()).isEqualTo(0);
+  }
+
+  @Test
+  public void twoCommentsWithChildRelationship() {
+    final List<Comment> commentList = createCommentList("41", "42");
+    final var singleCommentChildList = List.of(new CommentChild("97", "41", "42"));
+
+    final List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(commentList, singleCommentChildList);
+
+    assertThat(actualCommentNodes.size()).isEqualTo(1);
+    assertThat(actualCommentNodes.getFirst().getComment())
+        .extracting("id", "body", "documentId")
+        .containsExactly("41", "Test comment 41", "97");
+    assertThat(actualCommentNodes.getFirst().getChildren().size()).isEqualTo(1);
+    assertThat(actualCommentNodes.getFirst().getChildren().getFirst().getComment())
+        .extracting("id", "body", "documentId")
+        .containsExactly("42", "Test comment 42", "97");
+  }
+
+  @Test
+  public void chainOfComments() {
+    final List<Comment> commentList = createCommentList("41", "43", "46", "49");
+    final var commentChildList = List.of(
+        new CommentChild("97", "41", "43"),
+        new CommentChild("97", "43", "46"),
+        new CommentChild("97", "46", "49")
+    );
+
+    final List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(commentList, commentChildList);
+
+    final Comment firstLevel = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41")).getComment();
+    assertThat(firstLevel)
+        .extracting("id", "body", "documentId")
+        .containsExactly("41", "Test comment 41", "97");
+    final Comment secondLevel = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41", "43")).getComment();
+    assertThat(secondLevel)
+        .extracting("id", "body", "documentId")
+        .containsExactly("43", "Test comment 43", "97");
+    final Comment thirdLevel = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41", "43", "46")).getComment();
+    assertThat(thirdLevel)
+        .extracting("id", "body", "documentId")
+        .containsExactly("46", "Test comment 46", "97");
+    final Comment fourthLevel = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41", "43", "46", "49")).getComment();
+    assertThat(fourthLevel)
+        .extracting("id", "body", "documentId")
+        .containsExactly("49", "Test comment 49", "97");
+  }
+
+  @Test
+  public void treeOfComments() {
+    final List<Comment> commentList = createCommentList("43", "49", "50", "59", "66", "68");
+    final var commentChildList = List.of(
+        new CommentChild("97", "49", "50"),
+        new CommentChild("97", "49", "59"),
+        new CommentChild("97", "50", "66"),
+        new CommentChild("97", "59", "68")
+    );
+
+    final List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(commentList, commentChildList);
+
+    final Comment firstLevelFirstComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("43")).getComment();
+    assertThat(firstLevelFirstComment)
+        .extracting("id", "body", "documentId")
+        .containsExactly("43", "Test comment 43", "97");
+    final Comment firstLevelSecond = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("49")).getComment();
+    assertThat(firstLevelSecond)
+        .extracting("id", "body", "documentId")
+        .containsExactly("49", "Test comment 49", "97");
+    final Comment secondLevelFirstComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("49", "50")).getComment();
+    assertThat(secondLevelFirstComment)
+        .extracting("id", "body", "documentId")
+        .containsExactly("50", "Test comment 50", "97");
+    final Comment secondLevelSecondComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("49", "59")).getComment();
+    assertThat(secondLevelSecondComment)
+        .extracting("id", "body", "documentId")
+        .containsExactly("59", "Test comment 59", "97");
+    final Comment thirdLevelFirstComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("49", "50", "66")).getComment();
+    assertThat(thirdLevelFirstComment)
+        .extracting("id", "body", "documentId")
+        .containsExactly("66", "Test comment 66", "97");
+    final Comment thirdLevelSecondComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("49", "59", "68")).getComment();
+    assertThat(thirdLevelSecondComment)
+        .extracting("id", "body", "documentId")
+        .containsExactly("68", "Test comment 68", "97");
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideCommentChildIds")
+  public void gracefullyHandlesInvalidChildComments(final String parentId, final String childId) {
+    final List<Comment> commentList = createCommentList("41", "42", "43");
+    final var commentChildList = List.of(
+        new CommentChild("97", "41", "42"),
+        new CommentChild("97", "42", "43"),
+        new CommentChild("97", parentId, childId)
+    );
+
+    final List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(commentList, commentChildList);
+
+    assertThat(actualCommentNodes.size()).isEqualTo(1);
+    final CommentNode firstLevelFirstComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41"));
+    assertThat(firstLevelFirstComment.getChildren().size()).isEqualTo(1);
+    assertThat(firstLevelFirstComment.getComment())
+        .extracting("id", "body", "documentId")
+        .containsExactly("41", "Test comment 41", "97");
+    final CommentNode firstLevelSecond = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41", "42"));
+    assertThat(firstLevelSecond.getChildren().size()).isEqualTo(1);
+    assertThat(firstLevelSecond.getComment())
+        .extracting("id", "body", "documentId")
+        .containsExactly("42", "Test comment 42", "97");
+    final CommentNode secondLevelFirstComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41", "42", "43"));
+    assertThat(secondLevelFirstComment.getComment())
+        .extracting("id", "body", "documentId")
+        .containsExactly("43", "Test comment 43", "97");
+  }
+
+  private static Stream<Arguments> provideCommentChildIds() {
+    return Stream.of(
+        Arguments.of("50", "51"),
+        Arguments.of("41", "51"),
+        Arguments.of("50", "43")
+    );
+  }
+
+  private CommentNode getCommentNodeWithIdAtEachLevel(final List<CommentNode> commentNodes, final List<String> levelIds) {
+    if (levelIds.size() == 1) {
+      return getCommentNodeWithId(commentNodes, levelIds.getFirst());
+    } else {
+      return getCommentNodeWithIdAtEachLevel(getCommentNodeWithId(commentNodes, levelIds.getFirst()).getChildren(), levelIds.subList(1, levelIds.size()));
     }
+  }
 
-    @Test
-    public void oneCommentNoChildren() {
-        List<Comment> oneCommentList = createCommentList("41");
-        List<CommentChild> emptyCommentChildList = Collections.emptyList();
-
-        List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(oneCommentList, emptyCommentChildList);
-
-        assertThat(actualCommentNodes.size()).isEqualTo(1);
-        assertThat(actualCommentNodes.getFirst().getComment())
-                .extracting("id", "body", "documentId")
-                .containsExactly("41", "Test comment 41", "97");
-        assertThat(actualCommentNodes.getFirst().getChildren().size()).isEqualTo(0);
+  private CommentNode getCommentNodeWithId(final List<CommentNode> commentNodes, final String id) {
+    final Optional<CommentNode> first = commentNodes.stream().filter(commentNode -> commentNode.getComment().getId().equals(id)).findFirst();
+    if (first.isEmpty()) {
+      fail("The comment with ID \"" + id + "\" was not present at this level of the comment tree.");
     }
+    return first.get();
+  }
 
-    @Test
-    public void twoCommentsWithChildRelationship() {
-        List<Comment> commentList = createCommentList("41", "42");
-        var singleCommentChildList = List.of(new CommentChild("97", "41", "42"));
-
-        List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(commentList, singleCommentChildList);
-
-        assertThat(actualCommentNodes.size()).isEqualTo(1);
-        assertThat(actualCommentNodes.getFirst().getComment())
-                .extracting("id", "body", "documentId")
-                .containsExactly("41", "Test comment 41", "97");
-        assertThat(actualCommentNodes.getFirst().getChildren().size()).isEqualTo(1);
-        assertThat(actualCommentNodes.getFirst().getChildren().getFirst().getComment())
-                .extracting("id", "body", "documentId")
-                .containsExactly("42", "Test comment 42", "97");
+  private List<Comment> createCommentList(final String... commentIds) {
+    final List<Comment> commentList = new ArrayList<>();
+    for (final String commentId : commentIds) {
+      commentList.add(Comment.builder()
+          .id(commentId)
+          .documentId("97")
+          .createdDate(LocalDateTime.of(2024, Month.AUGUST, 3, 4, 23))
+          .modifiedDate(LocalDateTime.of(2024, Month.AUGUST, 3, 4, 27))
+          .body("Test comment " + commentId)
+          .creator("1")
+          .build());
     }
-
-    @Test
-    public void chainOfComments() {
-        List<Comment> commentList = createCommentList("41", "43", "46", "49");
-        var commentChildList = List.of(
-                new CommentChild("97", "41", "43"),
-                new CommentChild("97", "43", "46"),
-                new CommentChild("97", "46", "49")
-        );
-
-        List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(commentList, commentChildList);
-
-        Comment firstLevel = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41")).getComment();
-        assertThat(firstLevel)
-                .extracting("id", "body", "documentId")
-                .containsExactly("41", "Test comment 41", "97");
-        Comment secondLevel = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41", "43")).getComment();
-        assertThat(secondLevel)
-                .extracting("id", "body", "documentId")
-                .containsExactly("43", "Test comment 43", "97");
-        Comment thirdLevel = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41", "43", "46")).getComment();
-        assertThat(thirdLevel)
-                .extracting("id", "body", "documentId")
-                .containsExactly("46", "Test comment 46", "97");
-        Comment fourthLevel = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41", "43", "46", "49")).getComment();
-        assertThat(fourthLevel)
-                .extracting("id", "body", "documentId")
-                .containsExactly("49", "Test comment 49", "97");
-    }
-
-    @Test
-    public void treeOfComments() {
-        List<Comment> commentList = createCommentList("43", "49", "50", "59", "66", "68");
-        var commentChildList = List.of(
-                new CommentChild("97", "49", "50"),
-                new CommentChild("97", "49", "59"),
-                new CommentChild("97", "50", "66"),
-                new CommentChild("97", "59", "68")
-        );
-
-        List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(commentList, commentChildList);
-
-        Comment firstLevelFirstComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("43")).getComment();
-        assertThat(firstLevelFirstComment)
-                .extracting("id", "body", "documentId")
-                .containsExactly("43", "Test comment 43", "97");
-        Comment firstLevelSecond = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("49")).getComment();
-        assertThat(firstLevelSecond)
-                .extracting("id", "body", "documentId")
-                .containsExactly("49", "Test comment 49", "97");
-        Comment secondLevelFirstComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("49", "50")).getComment();
-        assertThat(secondLevelFirstComment)
-                .extracting("id", "body", "documentId")
-                .containsExactly("50", "Test comment 50", "97");
-        Comment secondLevelSecondComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("49", "59")).getComment();
-        assertThat(secondLevelSecondComment)
-                .extracting("id", "body", "documentId")
-                .containsExactly("59", "Test comment 59", "97");
-        Comment thirdLevelFirstComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("49", "50", "66")).getComment();
-        assertThat(thirdLevelFirstComment)
-                .extracting("id", "body", "documentId")
-                .containsExactly("66", "Test comment 66", "97");
-        Comment thirdLevelSecondComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("49", "59", "68")).getComment();
-        assertThat(thirdLevelSecondComment)
-                .extracting("id", "body", "documentId")
-                .containsExactly("68", "Test comment 68", "97");
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideCommentChildIds")
-    public void gracefullyHandlesInvalidChildComments(String parentId, String childId) {
-        List<Comment> commentList = createCommentList("41", "42", "43");
-        var commentChildList = List.of(
-                new CommentChild("97", "41", "42"),
-                new CommentChild("97", "42", "43"),
-                new CommentChild("97", parentId, childId)
-        );
-
-        List<CommentNode> actualCommentNodes = CommentGraph.buildGraph(commentList, commentChildList);
-
-        assertThat(actualCommentNodes.size()).isEqualTo(1);
-        CommentNode firstLevelFirstComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41"));
-        assertThat(firstLevelFirstComment.getChildren().size()).isEqualTo(1);
-        assertThat(firstLevelFirstComment.getComment())
-                .extracting("id", "body", "documentId")
-                .containsExactly("41", "Test comment 41", "97");
-        CommentNode firstLevelSecond = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41", "42"));
-        assertThat(firstLevelSecond.getChildren().size()).isEqualTo(1);
-        assertThat(firstLevelSecond.getComment())
-                .extracting("id", "body", "documentId")
-                .containsExactly("42", "Test comment 42", "97");
-        CommentNode secondLevelFirstComment = getCommentNodeWithIdAtEachLevel(actualCommentNodes, List.of("41", "42", "43"));
-        assertThat(secondLevelFirstComment.getComment())
-                .extracting("id", "body", "documentId")
-                .containsExactly("43", "Test comment 43", "97");
-    }
-
-    private static Stream<Arguments> provideCommentChildIds() {
-        return Stream.of(
-                Arguments.of("50", "51"),
-                Arguments.of("41", "51"),
-                Arguments.of("50", "43")
-        );
-    }
-
-    private CommentNode getCommentNodeWithIdAtEachLevel(List<CommentNode> commentNodes, List<String> levelIds) {
-        if (levelIds.size() == 1) {
-            return getCommentNodeWithId(commentNodes, levelIds.getFirst());
-        } else {
-            return getCommentNodeWithIdAtEachLevel(getCommentNodeWithId(commentNodes, levelIds.getFirst()).getChildren(), levelIds.subList(1, levelIds.size()));
-        }
-    }
-
-    private CommentNode getCommentNodeWithId(List<CommentNode> commentNodes, String id) {
-        Optional<CommentNode> first = commentNodes.stream().filter(commentNode -> commentNode.getComment().getId().equals(id)).findFirst();
-        if (first.isEmpty()) {
-            fail("The comment with ID \"" + id + "\" was not present at this level of the comment tree.");
-        }
-        return first.get();
-    }
-
-    private List<Comment> createCommentList(String... commentIds) {
-        List<Comment> commentList = new ArrayList<>();
-        for (String commentId : commentIds) {
-            commentList.add(Comment.builder()
-                    .id(commentId)
-                    .documentId("97")
-                    .createdDate(LocalDateTime.of(2024, Month.AUGUST, 3, 4, 23))
-                    .modifiedDate(LocalDateTime.of(2024, Month.AUGUST, 3, 4, 27))
-                    .body("Test comment " + commentId)
-                    .creator("1")
-                    .build());
-        }
-        return commentList;
-    }
+    return commentList;
+  }
 }
--- src/test/java/org/jono/medicalmodelsservice/utils/ListUtilsTest.java
+++ src/test/java/org/jono/medicalmodelsservice/utils/ListUtilsTest.java
@@ -18,98 +18,98 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ListUtilsTest {
 
-    private static final LocalDateTime TEST_DATE = LocalDateTime.of(2024, Month.AUGUST, 3, 4, 23);
+  private static final LocalDateTime TEST_DATE = LocalDateTime.of(2024, Month.AUGUST, 3, 4, 23);
 
-    @Nested
-    class DeduplicateTests {
-        @ParameterizedTest
-        @MethodSource("deduplicateStrings")
-        void duplicateStrings(List<String> inputList, List<String> expectedList) {
-            List<String> noDuplicates = ListUtils.deduplicate(inputList);
-            assertThat(noDuplicates).isEqualTo(expectedList);
-        }
-
-        @ParameterizedTest
-        @MethodSource("deduplicateIntegers")
-        void duplicateIntegers(List<Integer> inputList, List<Integer> expectedList) {
-            List<Integer> noDuplicates = ListUtils.deduplicate(inputList);
-            assertThat(noDuplicates).isEqualTo(expectedList);
-        }
-
-        private static Stream<Arguments> deduplicateStrings() {
-            return Stream.of(
-                    Arguments.of(List.of("a", "b", "a"), List.of("a", "b")),
-                    Arguments.of(List.of("a", "b", "a", "b"), List.of("a", "b")),
-                    Arguments.of(List.of("a", "a"), List.of("a")),
-                    Arguments.of(List.of("a", "b", "b"), List.of("a", "b")),
-                    Arguments.of(List.of("a", "A"), List.of("a", "A")),
-                    Arguments.of(List.of("A", "a"), List.of("A", "a")),
-                    Arguments.of(List.of("aa", "a"), List.of("aa", "a")),
-                    Arguments.of(List.of(), List.of())
-            );
-        }
-
-        private static Stream<Arguments> deduplicateIntegers() {
-            return Stream.of(
-                    Arguments.of(List.of(1, 2, 2), List.of(1, 2)),
-                    Arguments.of(List.of(1, 2), List.of(1, 2)),
-                    Arguments.of(List.of(1, 2, 1, 2), List.of(1, 2)),
-                    Arguments.of(List.of(1, 2, 1), List.of(1, 2)),
-                    Arguments.of(List.of(1, 2, 3), List.of(1, 2, 3)),
-                    Arguments.of(List.of(1, 2, 2, 3), List.of(1, 2, 3)),
-                    Arguments.of(List.of(1), List.of(1)),
-                    Arguments.of(List.of(), List.of())
-            );
-        }
+  @Nested
+  class DeduplicateTests {
+    @ParameterizedTest
+    @MethodSource("deduplicateStrings")
+    void duplicateStrings(final List<String> inputList, final List<String> expectedList) {
+      final List<String> noDuplicates = ListUtils.deduplicate(inputList);
+      assertThat(noDuplicates).isEqualTo(expectedList);
     }
 
-    @Nested
-    class ListToMapOfIdToItemTests {
-        public static final Comment TEST_COMMENT_1 = new Comment("1", "100", "1001", "test body", TEST_DATE, TEST_DATE);
-        public static final Comment TEST_COMMENT_2 = new Comment("2", "100", "1001", "test body", TEST_DATE, TEST_DATE);
-
-        @Test
-        public void listOfLength0() {
-            Map<String, Comment> map = ListUtils.listToMapOfIdToItem(List.of(), Comment::getId);
-
-            assertThat(map.size()).isEqualTo(0);
-        }
-
-        @Test
-        public void listOfLength1() {
-            Map<String, Comment> map = ListUtils.listToMapOfIdToItem(List.of(TEST_COMMENT_1), Comment::getId);
-
-            assertThat(map.size()).isEqualTo(1);
-            assertThat(map.get("1")).isEqualTo(TEST_COMMENT_1);
-        }
-
-        @Test
-        public void listOfLength2() {
-            Map<String, Comment> map = ListUtils.listToMapOfIdToItem(List.of(TEST_COMMENT_1, TEST_COMMENT_2), Comment::getId);
-
-            assertThat(map.size()).isEqualTo(2);
-            assertThat(map.get("1")).isEqualTo(TEST_COMMENT_1);
-            assertThat(map.get("2")).isEqualTo(TEST_COMMENT_2);
-        }
-
-        @Test
-        public void nullList() {
-            assertThatThrownBy(() -> ListUtils.listToMapOfIdToItem(null, Comment::getId))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
-
-        @Test
-        public void nullId() {
-            Comment nullIdComment = new Comment(null, "100", "1001", "test body", TEST_DATE, TEST_DATE);
-
-            assertThatThrownBy(() -> ListUtils.listToMapOfIdToItem(List.of(nullIdComment), Comment::getId))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
-
-        @Test
-        public void idFunctionReturnsNull() {
-            assertThatThrownBy(() -> ListUtils.listToMapOfIdToItem(List.of(TEST_COMMENT_1), comment -> null))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
+    @ParameterizedTest
+    @MethodSource("deduplicateIntegers")
+    void duplicateIntegers(final List<Integer> inputList, final List<Integer> expectedList) {
+      final List<Integer> noDuplicates = ListUtils.deduplicate(inputList);
+      assertThat(noDuplicates).isEqualTo(expectedList);
     }
+
+    private static Stream<Arguments> deduplicateStrings() {
+      return Stream.of(
+          Arguments.of(List.of("a", "b", "a"), List.of("a", "b")),
+          Arguments.of(List.of("a", "b", "a", "b"), List.of("a", "b")),
+          Arguments.of(List.of("a", "a"), List.of("a")),
+          Arguments.of(List.of("a", "b", "b"), List.of("a", "b")),
+          Arguments.of(List.of("a", "A"), List.of("a", "A")),
+          Arguments.of(List.of("A", "a"), List.of("A", "a")),
+          Arguments.of(List.of("aa", "a"), List.of("aa", "a")),
+          Arguments.of(List.of(), List.of())
+      );
+    }
+
+    private static Stream<Arguments> deduplicateIntegers() {
+      return Stream.of(
+          Arguments.of(List.of(1, 2, 2), List.of(1, 2)),
+          Arguments.of(List.of(1, 2), List.of(1, 2)),
+          Arguments.of(List.of(1, 2, 1, 2), List.of(1, 2)),
+          Arguments.of(List.of(1, 2, 1), List.of(1, 2)),
+          Arguments.of(List.of(1, 2, 3), List.of(1, 2, 3)),
+          Arguments.of(List.of(1, 2, 2, 3), List.of(1, 2, 3)),
+          Arguments.of(List.of(1), List.of(1)),
+          Arguments.of(List.of(), List.of())
+      );
+    }
+  }
+
+  @Nested
+  class ListToMapOfIdToItemTests {
+    public static final Comment TEST_COMMENT_1 = new Comment("1", "100", "1001", "test body", TEST_DATE, TEST_DATE);
+    public static final Comment TEST_COMMENT_2 = new Comment("2", "100", "1001", "test body", TEST_DATE, TEST_DATE);
+
+    @Test
+    public void listOfLength0() {
+      final Map<String, Comment> map = ListUtils.listToMapOfIdToItem(List.of(), Comment::getId);
+
+      assertThat(map.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void listOfLength1() {
+      final Map<String, Comment> map = ListUtils.listToMapOfIdToItem(List.of(TEST_COMMENT_1), Comment::getId);
+
+      assertThat(map.size()).isEqualTo(1);
+      assertThat(map.get("1")).isEqualTo(TEST_COMMENT_1);
+    }
+
+    @Test
+    public void listOfLength2() {
+      final Map<String, Comment> map = ListUtils.listToMapOfIdToItem(List.of(TEST_COMMENT_1, TEST_COMMENT_2), Comment::getId);
+
+      assertThat(map.size()).isEqualTo(2);
+      assertThat(map.get("1")).isEqualTo(TEST_COMMENT_1);
+      assertThat(map.get("2")).isEqualTo(TEST_COMMENT_2);
+    }
+
+    @Test
+    public void nullList() {
+      assertThatThrownBy(() -> ListUtils.listToMapOfIdToItem(null, Comment::getId))
+          .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void nullId() {
+      final Comment nullIdComment = new Comment(null, "100", "1001", "test body", TEST_DATE, TEST_DATE);
+
+      assertThatThrownBy(() -> ListUtils.listToMapOfIdToItem(List.of(nullIdComment), Comment::getId))
+          .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void idFunctionReturnsNull() {
+      assertThatThrownBy(() -> ListUtils.listToMapOfIdToItem(List.of(TEST_COMMENT_1), comment -> null))
+          .isInstanceOf(IllegalArgumentException.class);
+    }
+  }
 }
