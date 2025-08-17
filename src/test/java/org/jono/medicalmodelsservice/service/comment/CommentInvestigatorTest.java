e791dcea6a436401184500f4ceb990f6347cde4c
JonathonClifford <jono.cliff@gmail.com>
2025-08-17 12:19:48 +0000

    test: MM-91 First CommentInvestigator test. (#45)
    Reviewed-on: https://gitea.busybunyip.com/medical-models/medical-models-service/pulls/45
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

---
+++ src/test/java/org/jono/medicalmodelsservice/service/comment/CommentInvestigatorTest.java
@@ -0,0 +1,21 @@
+package org.jono.medicalmodelsservice.service.comment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.jono.medicalmodelsservice.model.CommentChild;
+import org.jono.medicalmodelsservice.model.Tuple2;
+import org.jono.medicalmodelsservice.repository.jdbc.CommentChildRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class CommentInvestigatorTest {
+
+  @Test
+  void findNodesToDelete() {
+    CommentChildRepository commentChildRepository = Mockito.mock(CommentChildRepository.class);
+    var commentInvestigator = new CommentInvestigator(commentChildRepository);
+    CommentsToDelete commentsToDelete = commentInvestigator.findNodesToDelete("1", new Tuple2<>(List.of(new CommentChild("1", "11", "101")), List.of(new CommentChild("1", "11", "101"))));
+    assertThat(commentsToDelete.commentIds).containsExactly("1");
+  }
+}
