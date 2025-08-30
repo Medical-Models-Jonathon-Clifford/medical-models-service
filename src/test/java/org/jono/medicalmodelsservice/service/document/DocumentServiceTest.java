6c52425fc6fb2c84d1105af87c6accd22de6a26b
JonathonClifford <jono.cliff@gmail.com>
2025-08-30 07:46:49 +0000

    test: MM-92 Add tests for read, update and getAllNavigation methods. (#55)
    Reviewed-on: https://gitea.busybunyip.com/medical-models/medical-models-service/pulls/55
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

--- src/test/java/org/jono/medicalmodelsservice/service/document/DocumentServiceTest.java
+++ src/test/java/org/jono/medicalmodelsservice/service/document/DocumentServiceTest.java
@@ -6,8 +6,13 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 import org.jono.medicalmodelsservice.model.Document;
+import org.jono.medicalmodelsservice.model.DocumentChild;
+import org.jono.medicalmodelsservice.model.DocumentState;
+import org.jono.medicalmodelsservice.model.Tuple2;
+import org.jono.medicalmodelsservice.model.dto.DocumentDto;
 import org.jono.medicalmodelsservice.repository.jdbc.DocumentChildRepository;
 import org.jono.medicalmodelsservice.repository.jdbc.DocumentRepository;
 import org.junit.jupiter.api.Nested;
@@ -50,6 +55,57 @@ class DocumentServiceTest {
 
             assertThat(document).isNotNull();
             verify(documentChildRepository, times(0)).create(any(), any());
+        }
+    }
+
+    @Nested
+    class ReadDocument {
+        @Test
+        void shouldCallReadByIdOnDocRepo() {
+            when(documentRepository.findById(any())).thenReturn(Optional.of(new Document()));
+
+            final Optional<Document> document = documentService.readDocument("1");
+
+            assertThat(document).isNotNull();
+            verify(documentRepository).findById("1");
+        }
+    }
+
+    @Nested
+    class UpdateDocument {
+
+        @Test
+        void shouldCallUpdateByIdOnDocRepo() {
+            when(documentRepository.updateById(any(), any())).thenReturn(new Document());
+
+            final DocumentDto testDocumentDto = new DocumentDto("test title", "test body", DocumentState.ACTIVE);
+            final Document document =
+                    documentService.updateDocument("1", testDocumentDto);
+
+            assertThat(document).isNotNull();
+            verify(documentRepository).updateById("1", testDocumentDto);
+        }
+    }
+
+    @Nested
+    class GetAllNavigation {
+        @Test
+        void shouldCallGetAllOnDocRepo() {
+            when(documentRepository.getDocsAndDocChildren())
+                    .thenReturn(
+                            new Tuple2<>(
+                                    List.of(new DocumentChild("1", "11")),
+                                    List.of(Document.builder().id("1").build(), Document.builder().id("11").build())
+                            )
+                    );
+
+            final List<DocumentNode> actual = documentService.getAllNavigation();
+
+            assertThat(actual).isNotNull();
+            assertThat(actual).hasSize(1);
+            assertThat(actual.getFirst().getDocument().getId()).isEqualTo("1");
+            assertThat(actual.getFirst().getChildren()).hasSize(1);
+            assertThat(actual.getFirst().getChildren().getFirst().getDocument().getId()).isEqualTo("11");
         }
     }
 }
