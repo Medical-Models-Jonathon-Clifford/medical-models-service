53687e53dda5691d7123457c143aa3aced30712f
JonathonClifford <jono.cliff@gmail.com>
2025-09-04 01:52:47 +0000

    test: MM-92 Happy path test for each CommentInvestigator path. (#71)
    Reviewed-on: https://gitea.busybunyip.com/medical-models/medical-models-service/pulls/71
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

--- src/main/java/org/jono/medicalmodelsservice/model/CommentRelationship.java
+++ src/main/java/org/jono/medicalmodelsservice/model/CommentRelationship.java
@@ -1,12 +1,16 @@
 package org.jono.medicalmodelsservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.jono.medicalmodelsservice.service.NodeRelationship;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("comment_relationship")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentRelationship implements NodeRelationship {
     @Id
     private String id;
--- src/test/java/org/jono/medicalmodelsservice/service/comment/CommentInvestigatorTest.java
+++ src/test/java/org/jono/medicalmodelsservice/service/comment/CommentInvestigatorTest.java
@@ -1,25 +1,87 @@
 package org.jono.medicalmodelsservice.service.comment;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+import java.util.Collections;
 import java.util.List;
 import org.jono.medicalmodelsservice.model.CommentRelationship;
 import org.jono.medicalmodelsservice.repository.jdbc.CommentRelationshipRepository;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class CommentInvestigatorTest {
 
-    @Test
-    void findNodesToDelete() {
-        final CommentRelationshipRepository commentRelationshipRepository = Mockito.mock(
-                CommentRelationshipRepository.class);
-        final var commentInvestigator = new CommentInvestigator(commentRelationshipRepository);
-        final CommentsToDelete commentsToDelete =
-                commentInvestigator.findNodesToDelete("1",
-                                                      List.of(new CommentRelationship("1", "11", "101")),
-                                                      List.of(new CommentRelationship("1", "11", "101"))
-                );
-        assertThat(commentsToDelete.commentIds).containsExactly("1");
+    @Nested
+    class RootNode {
+
+        @Test
+        void shouldDeleteRootNode() {
+            final CommentRelationshipRepository commentChildRepository = Mockito.mock(
+                    CommentRelationshipRepository.class);
+            final var commentInvestigator = new CommentInvestigator(commentChildRepository);
+            final CommentsToDelete commentsToDelete =
+                    commentInvestigator.findNodesToDelete("1",
+                                                          List.of(new CommentRelationship("1", "11", "101")),
+                                                          Collections.emptyList());
+            assertThat(commentsToDelete.commentIds).containsExactly("1");
+            assertThat(commentsToDelete.childCommentIds).isEmpty();
+        }
+    }
+
+    @Nested
+    class InternalNode {
+
+        @Test
+        void shouldDeleteInternalNode() {
+            final CommentRelationshipRepository commentChildRepository = Mockito.mock(
+                    CommentRelationshipRepository.class);
+            final var commentInvestigator = new CommentInvestigator(commentChildRepository);
+            final CommentsToDelete commentsToDelete =
+                    commentInvestigator.findNodesToDelete("1",
+                                                          List.of(new CommentRelationship("1", "11", "101")),
+                                                          List.of(new CommentRelationship("1", "11", "101")));
+            assertThat(commentsToDelete.commentIds).containsExactly("1");
+            assertThat(commentsToDelete.childCommentIds).isEmpty();
+        }
+    }
+
+    @Nested
+    class LeafNode {
+
+        @Test
+        void shouldDeleteLeafNode() {
+            final CommentRelationshipRepository commentChildRepository = Mockito.mock(
+                    CommentRelationshipRepository.class);
+            when(commentChildRepository.findLeafNodesParentConnection("1")).thenReturn(
+                    new CommentRelationship("1", "1", "11", "101"));
+            final var commentInvestigator = new CommentInvestigator(commentChildRepository);
+            final CommentsToDelete commentsToDelete =
+                    commentInvestigator.findNodesToDelete("1",
+                                                          Collections.emptyList(),
+                                                          List.of(new CommentRelationship("1", "11", "101")));
+            assertThat(commentsToDelete.commentIds).containsExactly("1");
+            assertThat(commentsToDelete.childCommentIds).containsExactly("1");
+        }
+    }
+
+    @Nested
+    class IsolatedNode {
+
+        @Test
+        void shouldDeleteIsolatedNode() {
+            final CommentRelationshipRepository commentChildRepository = Mockito.mock(
+                    CommentRelationshipRepository.class);
+            when(commentChildRepository.findLeafNodesParentConnection("1")).thenReturn(
+                    new CommentRelationship("1", "1", "11", "101"));
+            final var commentInvestigator = new CommentInvestigator(commentChildRepository);
+            final CommentsToDelete commentsToDelete =
+                    commentInvestigator.findNodesToDelete("1",
+                                                          Collections.emptyList(),
+                                                          Collections.emptyList());
+            assertThat(commentsToDelete.commentIds).containsExactly("1");
+            assertThat(commentsToDelete.childCommentIds).isEmpty();
+        }
     }
 }
