2d3f1c5ee30f1904a863a7a45aa30a729b779e8e
JonathonClifford <jono.cliff@gmail.com>
2025-09-06 11:01:33 +0000

    test: MM-91 Write the remaining test cases to increase test coverage of CommentInvestigatorTest to 100%. (#87)
    Reviewed-on: https://gitea.busybunyip.com/medical-models/medical-models-service/pulls/87
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

--- src/test/java/org/jono/medicalmodelsservice/service/comment/CommentInvestigatorTest.java
+++ src/test/java/org/jono/medicalmodelsservice/service/comment/CommentInvestigatorTest.java
@@ -3,25 +3,53 @@ package org.jono.medicalmodelsservice.service.comment;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.Collections;
+import java.util.List;
 import org.jono.medicalmodelsservice.model.CommentRelationship;
+import org.jono.medicalmodelsservice.repository.jdbc.CommentRelationshipCrudRepository;
 import org.jono.medicalmodelsservice.repository.jdbc.CommentRelationshipRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class CommentInvestigatorTest {
 
+    private static final String DOC_ID = "1";
+
+    private CommentRelationshipCrudRepository crudRepository;
+    private CommentInvestigator commentInvestigator;
+
+    @BeforeEach
+    void setUp() {
+        crudRepository = Mockito.mock(CommentRelationshipCrudRepository.class);
+        final var repository = new CommentRelationshipRepository(crudRepository);
+        commentInvestigator = new CommentInvestigator(repository);
+    }
+
     @Nested
     class RootNode {
 
         @Test
-        void shouldDeleteRootNode() {
-            final CommentRelationshipRepository commentChildRepository = Mockito.mock(
-                    CommentRelationshipRepository.class);
-            final var commentInvestigator = new CommentInvestigator(commentChildRepository);
-            final CommentsToDelete commentsToDelete = commentInvestigator.findCommentsToDelete("1");
-            assertThat(commentsToDelete.commentIds()).containsExactly("1");
-            assertThat(commentsToDelete.commentRelationshipIds()).isEmpty();
+        void shouldDeleteSingleRelationship() {
+            when(crudRepository.findAllByChildCommentId("101")).thenReturn(Collections.emptyList());
+            when(crudRepository.findAllByParentCommentId("101")).thenReturn(
+                    List.of(new CommentRelationship("1", DOC_ID, "101", "102")));
+            final CommentsToDelete commentsToDelete = commentInvestigator.findCommentsToDelete("101");
+            assertThat(commentsToDelete.commentIds()).containsExactly("101", "102");
+            assertThat(commentsToDelete.commentRelationshipIds()).containsExactly("1");
+        }
+
+        @Test
+        void shouldDeleteTwoRelationships() {
+            when(crudRepository.findAllByChildCommentId("101")).thenReturn(Collections.emptyList());
+            when(crudRepository.findAllByParentCommentId("101")).thenReturn(
+                    List.of(new CommentRelationship("1", DOC_ID, "101", "102")));
+            when(crudRepository.findAllByParentCommentId("102")).thenReturn(
+                    List.of(new CommentRelationship("2", DOC_ID, "102", "103")));
+            final CommentsToDelete commentsToDelete = commentInvestigator.findCommentsToDelete("101");
+            assertThat(commentsToDelete.commentIds()).containsExactly("101", "102", "103");
+            assertThat(commentsToDelete.commentRelationshipIds()).containsExactlyInAnyOrder("1", "2");
         }
     }
 
@@ -30,12 +58,13 @@ class CommentInvestigatorTest {
 
         @Test
         void shouldDeleteInternalNode() {
-            final CommentRelationshipRepository commentChildRepository = Mockito.mock(
-                    CommentRelationshipRepository.class);
-            final var commentInvestigator = new CommentInvestigator(commentChildRepository);
-            final CommentsToDelete commentsToDelete = commentInvestigator.findCommentsToDelete("1");
-            assertThat(commentsToDelete.commentIds()).containsExactly("1");
-            assertThat(commentsToDelete.commentRelationshipIds()).isEmpty();
+            when(crudRepository.findAllByChildCommentId("102")).thenReturn(
+                    List.of(new CommentRelationship("2", DOC_ID, "101", "102")));
+            when(crudRepository.findAllByParentCommentId("102")).thenReturn(
+                    List.of(new CommentRelationship("1", DOC_ID, "102", "103")));
+            final CommentsToDelete commentsToDelete = commentInvestigator.findCommentsToDelete("102");
+            assertThat(commentsToDelete.commentIds()).containsExactlyInAnyOrder("102", "103");
+            assertThat(commentsToDelete.commentRelationshipIds()).containsExactlyInAnyOrder("1", "2");
         }
     }
 
@@ -44,15 +73,16 @@ class CommentInvestigatorTest {
 
         @Test
         void shouldDeleteLeafNode() {
-            final CommentRelationshipRepository commentChildRepository = Mockito.mock(
-                    CommentRelationshipRepository.class);
-            when(commentChildRepository.findRelationshipToParent("1")).thenReturn(
-                    new CommentRelationship("1", "1", "11", "101"));
-            final var commentInvestigator = new CommentInvestigator(commentChildRepository);
-            final CommentsToDelete commentsToDelete = commentInvestigator.findCommentsToDelete("1");
-            // TODO: Fix this test.
-            // assertThat(commentsToDelete.commentIds()).containsExactly("1");
-            // assertThat(commentsToDelete.commentRelationshipIds()).containsExactly("1");
+            when(crudRepository.findAllByChildCommentId("102")).thenReturn(
+                    List.of(new CommentRelationship("1", DOC_ID, "101", "102")));
+            when(crudRepository.findAllByChildCommentId("103")).thenReturn(
+                    List.of(new CommentRelationship("2", DOC_ID, "102", "103")));
+            when(crudRepository.findFirstByChildCommentId("103")).thenReturn(
+                    new CommentRelationship("2", DOC_ID, "102", "103"));
+            when(crudRepository.findAllByParentCommentId("103")).thenReturn(Collections.emptyList());
+            final CommentsToDelete commentsToDelete = commentInvestigator.findCommentsToDelete("103");
+            assertThat(commentsToDelete.commentIds()).containsExactly("103");
+            assertThat(commentsToDelete.commentRelationshipIds()).containsExactly("2");
         }
     }
 
@@ -61,13 +91,10 @@ class CommentInvestigatorTest {
 
         @Test
         void shouldDeleteIsolatedNode() {
-            final CommentRelationshipRepository commentChildRepository = Mockito.mock(
-                    CommentRelationshipRepository.class);
-            when(commentChildRepository.findRelationshipToParent("1")).thenReturn(
-                    new CommentRelationship("1", "1", "11", "101"));
-            final var commentInvestigator = new CommentInvestigator(commentChildRepository);
-            final CommentsToDelete commentsToDelete = commentInvestigator.findCommentsToDelete("1");
-            assertThat(commentsToDelete.commentIds()).containsExactly("1");
+            when(crudRepository.findAllByChildCommentId("101")).thenReturn(Collections.emptyList());
+            when(crudRepository.findAllByParentCommentId("101")).thenReturn(Collections.emptyList());
+            final CommentsToDelete commentsToDelete = commentInvestigator.findCommentsToDelete("101");
+            assertThat(commentsToDelete.commentIds()).containsExactly("101");
             assertThat(commentsToDelete.commentRelationshipIds()).isEmpty();
         }
     }
