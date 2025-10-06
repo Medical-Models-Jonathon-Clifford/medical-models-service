968c31782ec10dd59e1262274ba1ac57abd642ca
JonathonClifford <jono.cliff@gmail.com>
2025-10-06 07:06:50 +0000

    test: MM-100 SupportController integration tests. (#126)
    Reviewed-on: https://gitea.busybunyip.com/medical-models/medical-models-service/pulls/126
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

---
+++ src/test/java/org/jono/medicalmodelsservice/integration/SupportControllerIntTest.java
@@ -0,0 +1,271 @@
+package org.jono.medicalmodelsservice.integration;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jono.medicalmodelsservice.model.CompanySupportSearchParams;
+import org.jono.medicalmodelsservice.model.UserSupportSearchParams;
+import org.jono.medicalmodelsservice.utils.AuthenticationUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MinIOContainer;
+import org.testcontainers.containers.MySQLContainer;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
+@AutoConfigureMockMvc
+@Sql(scripts = {"/schema.sql", "/data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Import(IntegrationTestConfig.class)
+class SupportControllerIntTest {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String TEST_JWT = "Bearer " + TestJwtUtils.createTestJwtToken(
+            "rtrenneman",
+            "rtrenneman@example.com",
+            "2"
+    );
+
+
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.36");
+    static MinIOContainer minio = new MinIOContainer("minio/minio:RELEASE.2023-09-04T19-57-37Z");
+
+    @MockitoBean
+    AuthenticationUtils authUtils;
+
+    @BeforeAll
+    static void beforeAll() {
+        mysql.start();
+        minio.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mysql.stop();
+        minio.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("minio.endpoint", minio::getS3URL);
+        registry.add("minio.accessKey", minio::getUserName);
+        registry.add("minio.secretKey", minio::getPassword);
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnModelRankings() throws Exception {
+        this.mockMvc.perform(get("/support/models/ranking")
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(4)))
+                .andExpect(jsonPath("$[0].type", is("text")))
+                .andExpect(jsonPath("$[0].frequency", is(20)))
+                .andExpect(jsonPath("$[1].type", is("dielectric")))
+                .andExpect(jsonPath("$[1].frequency", is(14)))
+                .andExpect(jsonPath("$[2].type", is("half-life")))
+                .andExpect(jsonPath("$[2].frequency", is(13)))
+                .andExpect(jsonPath("$[3].type", is("image")))
+                .andExpect(jsonPath("$[3].frequency", is(2)))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnCompanyMetrics() throws Exception {
+        this.mockMvc.perform(get("/support/companies/metrics")
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.total", is(3)))
+                .andExpect(jsonPath("$.dailyCounts[0].date", is("2025-08-01")))
+                .andExpect(jsonPath("$.dailyCounts[0].newResources", is(1)))
+                .andExpect(jsonPath("$.dailyCounts[0].runningTotal", is(1)))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnUserMetrics() throws Exception {
+        this.mockMvc.perform(get("/support/users/metrics")
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.total", is(9)))
+                .andExpect(jsonPath("$.dailyCounts[0].date", is("2025-01-01")))
+                .andExpect(jsonPath("$.dailyCounts[0].newResources", is(1)))
+                .andExpect(jsonPath("$.dailyCounts[0].runningTotal", is(1)))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnDocumentMetrics() throws Exception {
+        this.mockMvc.perform(get("/support/documents/metrics")
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.total", is(13)))
+                .andExpect(jsonPath("$.dailyCounts[0].date", is("2025-01-01")))
+                .andExpect(jsonPath("$.dailyCounts[0].newResources", is(1)))
+                .andExpect(jsonPath("$.dailyCounts[0].runningTotal", is(1)))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnCommentMetricsForCompany() throws Exception {
+        this.mockMvc.perform(get("/support/comments/metrics")
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.total", is(11)))
+                .andExpect(jsonPath("$.dailyCounts[0].date", is("2025-01-25")))
+                .andExpect(jsonPath("$.dailyCounts[0].newResources", is(3)))
+                .andExpect(jsonPath("$.dailyCounts[0].runningTotal", is(3)))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnAllCompaniesFromEmptySearchParams() throws Exception {
+        // Returns all users when both search params are empty.
+        final var searchParams = new CompanySupportSearchParams("", "");
+        final var requestBody = objectMapper.writeValueAsString(searchParams);
+        this.mockMvc.perform(post("/support/companies/search")
+                                     .content(requestBody)
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].name", is("Medical Models IT Support Crowd")))
+                .andExpect(jsonPath("$[1].name", is("House MD Centre for Superheroes")))
+                .andExpect(jsonPath("$[2].name", is("Old Action Heroes M*A*S*H")))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnCompaniesMatchingNameSearchParam() throws Exception {
+        // Returns companies with names matching the search term "House".
+        final var searchParams = new CompanySupportSearchParams("House", "");
+        final var requestBody = objectMapper.writeValueAsString(searchParams);
+        this.mockMvc.perform(post("/support/companies/search")
+                                     .content(requestBody)
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is("2")))
+                .andExpect(jsonPath("$[0].name", is("House MD Centre for Superheroes")))
+                .andExpect(jsonPath("$[0].locationState", is("QLD")))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnCompaniesMatchingStateSearchParam() throws Exception {
+        // Returns companies with states matching the search term "NSW".
+        final var searchParams = new CompanySupportSearchParams("", "NSW");
+        final var requestBody = objectMapper.writeValueAsString(searchParams);
+        this.mockMvc.perform(post("/support/companies/search")
+                                     .content(requestBody)
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is("1")))
+                .andExpect(jsonPath("$[0].name", is("Medical Models IT Support Crowd")))
+                .andExpect(jsonPath("$[0].locationState", is("NSW")))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnCompaniesMatchingNameAndStateSearchParam() throws Exception {
+        // Returns companies with names and states matching the search terms "House" and "QLD".
+        final var searchParams = new CompanySupportSearchParams("House", "QLD");
+        final var requestBody = objectMapper.writeValueAsString(searchParams);
+        this.mockMvc.perform(post("/support/companies/search")
+                                     .content(requestBody)
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is("2")))
+                .andExpect(jsonPath("$[0].name", is("House MD Centre for Superheroes")))
+                .andExpect(jsonPath("$[0].locationState", is("QLD")))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnAllUsersWithNoSearchParams() throws Exception {
+        // Returns all users where the search term is empty.
+        final var searchParams = new UserSupportSearchParams("");
+        final var requestBody = objectMapper.writeValueAsString(searchParams);
+        this.mockMvc.perform(post("/support/users/search")
+                                     .content(requestBody)
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(9)))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "rtrenneman")
+    void shouldReturnUserMatchingSearchParam() throws Exception {
+        // Returns users with names matching the search term "Lisa".
+        final var searchParams = new UserSupportSearchParams("Lisa");
+        final var requestBody = objectMapper.writeValueAsString(searchParams);
+        this.mockMvc.perform(post("/support/users/search")
+                                     .content(requestBody)
+                                     .header("Authorization", TEST_JWT)
+                                     .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is("4")))
+                .andExpect(jsonPath("$[0].name", is("Dr. Lisa Cuddy")))
+                .andExpect(jsonPath("$[0].email", is("lcuddy@princetonplainsboro.org")))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+}
